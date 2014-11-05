5556cb605a4f47ebcf33aa7e3698a6bfb2df4d96
Laurent SALCEDO <lasalced@e1r10p17.42.fr>
2014-11-05 17:26:33 +0100

    lib

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lasalced <lasalced@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 11:45:11 by lasalced          #+#    #+#             */
-/*   Updated: 2014/11/04 15:33:10 by lasalced         ###   ########.fr       */
+/*   Updated: 2014/11/05 17:23:03 by lasalced         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 # define LIBFT_H
 # include <string.h>
 
+int		ft_isalpha(int c);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strdup(const char *s1);
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strncpy(char *dst, const char *src, size_t n);
 char	*ft_strcat(char *s1, const char *s2);
 char	*ft_strncat(char *s1, const char *s2, size_t n);
 char	*ft_strstr(const char *s1, const char *s2);
+char	*ft_strnstr(const char *s1, const char *s2, size_t n);
 size_t	ft_strlen(const char *str);
 
 #endif
