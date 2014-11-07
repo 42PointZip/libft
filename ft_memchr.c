c8b4b80e6a3ef68ce7aa13472024b1771b43762f
Laurent SALCEDO <lasalced@e1r9p15.42.fr>
2014-11-07 16:43:27 +0100

    chr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,28 +6,26 @@
 /*   By: lasalced <lasalced@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/06 16:25:10 by lasalced          #+#    #+#             */
-/*   Updated: 2014/11/07 13:54:46 by lasalced         ###   ########.fr       */
+/*   Updated: 2014/11/07 16:42:44 by lasalced         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <string.h>
+#include <stdio.h>
 
-void			*ft_memchr(const void *s, int c, size_t n)
+void				*ft_memchr(const void *s, int c, size_t n)
 {
-	const char	*d;
-	size_t		i;
+	unsigned char	*str;
+	unsigned char	ca;
+	size_t			i;
 
-	d = s;
 	i = 0;
-	if (n > 0)
-	{
-		while (n > i)
-		{
-			if (d[i] == (unsigned char)c)
-				return (&s + i);
-			i++;
-		}
-	}
-	return (NULL);
+	ca = (unsigned char)c;
+	str = (unsigned char *)s;
+	while (i != n && str[i] != ca && str)
+		i++;
+	if (i == n)
+		return (NULL);
+	return ((void *)s + i);
 }
