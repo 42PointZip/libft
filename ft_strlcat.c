0a167f806f1c1be102f599834cbfaa8a2b7e9bcf
Laurent SALCEDO <lasalced@e1r10p18.42.fr>
2014-11-08 12:46:42 +0100

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,13 +6,30 @@
 /*   By: lasalced <lasalced@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/07 17:13:33 by lasalced          #+#    #+#             */
-/*   Updated: 2014/11/07 17:43:23 by lasalced         ###   ########.fr       */
+/*   Updated: 2014/11/08 12:41:29 by lasalced         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "stdlib.h"
+#include "libft.h"
+#include <string.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	
+	size_t	len_dst;
+	size_t	len_src;
+	size_t	i;
+
+	i = 0;
+	len_dst = ft_strlen(dst);
+	len_src = ft_strlen(src);
+
+	if	(len_dst > size)
+		return (len_src + size);
+	while ((len_dst + i < size - 1) && src[i] != '\0')
+	{
+		dst[len_dst + i] = src[i];
+		i++;
+	}
+	dst[len_dst + i] = '\0';
+	return (len_src + len_dst);
 }
