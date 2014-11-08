b0e41617e5db2469bdea79ea23c011d8529acc56
Laurent SALCEDO <lasalced@e1r10p18.42.fr>
2014-11-08 12:46:09 +0100

    memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: lasalced <lasalced@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/07 17:05:33 by lasalced          #+#    #+#             */
-/*   Updated: 2014/11/07 17:13:49 by lasalced         ###   ########.fr       */
+/*   Updated: 2014/11/08 12:28:04 by lasalced         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,16 @@ int							ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	const unsigned char		*d1;
 	const unsigned char		*d2;
+	int						i;
 
+	i = 0;
 	d1 = s1;
 	d2 = s2;
 	while (n > 0)
 	{
-		if (*d1 != *d2)
-			return (*d1 - *d2);
-		d1++;
-		d2++;
+		if (d1[i] != d2[i])
+			return (d1[i] - d2[i]);
+		i++;
 		n--;
 	}
 	return (0);
