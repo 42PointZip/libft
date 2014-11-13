d3367536c415c7e0f3f50b954f98467184b0695b
Laurent SALCEDO <lasalced@e1r10p17.42.fr>
2014-11-13 15:43:10 +0100

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: lasalced <lasalced@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/05 12:32:00 by lasalced          #+#    #+#             */
-/*   Updated: 2014/11/05 13:18:51 by lasalced         ###   ########.fr       */
+/*   Updated: 2014/11/13 14:13:47 by lasalced         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,15 @@ int			ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
-	{
-		if (s1[i] - s2[i] < 0)
-			return (-1);
-		if (s1[i] - s2[i] > 0)
-			return (1);
+	if (n == 0)
+		return (0);
+	while ((s1[i] == s2[i]) && (s1[i] && s2[i]) && i < n)
 		i++;
-	}
-	return (0);
+	if (s1[i] == '\0' && s2[i])
+		return (-1);
+	if (s2[i] == '\0' && s1[i])
+		return (1);
+	if (i == n)
+		return (0);
+	return (s1[i] - s2[i]);
 }
