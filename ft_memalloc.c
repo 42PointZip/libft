520cf8725d7d207859ab42343ee642f8191502bf
Laurent SALCEDO <lasalced@e1r10p17.42.fr>
2014-11-14 13:55:06 +0100

    memalloc n42

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,17 +6,17 @@
 /*   By: lasalced <lasalced@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/14 12:04:07 by lasalced          #+#    #+#             */
-/*   Updated: 2014/11/14 13:14:14 by lasalced         ###   ########.fr       */
+/*   Updated: 2014/11/14 13:54:35 by lasalced         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-void	*ft_memalloc(size_t size)
+void				*ft_memalloc(size_t size)
 {
-	size_t	i;
-	unsigned char *ptr;
+	size_t			i;
+	unsigned char	*ptr;
 
 	i = 0;
 	ptr = (unsigned char *)malloc(size);
