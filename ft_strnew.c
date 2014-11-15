e2ee15aa642adf1362a3c415e000cc530587b501
Laurent SALCEDO <lasalced@e1r10p17.42.fr>
2014-11-15 14:23:51 +0100

    new

---
+++ ft_strnew.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lasalced <lasalced@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/15 14:10:33 by lasalced          #+#    #+#             */
+/*   Updated: 2014/11/15 14:21:44 by lasalced         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strnew(size_t size)
+{
+	char	*str;
+	size_t	i;
+
+	str = (char *)malloc(size);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		str[i] = '\0';
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
