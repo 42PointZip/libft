58a53784470595ea214a7cebc3bea361ce280668
Laurent SALCEDO <lasalced@e1r9p16.42.fr>
2014-11-17 12:28:12 +0100

    dub

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: lasalced <lasalced@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 11:15:32 by lasalced          #+#    #+#             */
-/*   Updated: 2014/11/10 13:25:36 by lasalced         ###   ########.fr       */
+/*   Updated: 2014/11/17 09:18:53 by lasalced         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strdup(const char *s1)
 {
 	char	*dest;
 
+	if (!s1)
+		return (NULL);
 	dest = (char *)malloc(sizeof(char) * ft_strlen(s1) + 1);
 	dest = ft_strcpy(dest, s1);
 	return (dest);
