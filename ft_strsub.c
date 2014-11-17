20fd1314cfdc943e84c524c8ee4c68dfce7c47cd
Laurent SALCEDO <lasalced@e1r9p16.42.fr>
2014-11-17 12:27:54 +0100

    sub

---
+++ ft_strsub.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lasalced <lasalced@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/17 10:51:31 by lasalced          #+#    #+#             */
+/*   Updated: 2014/11/17 12:25:49 by lasalced         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char				*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	size_t			j;
+	char			*temp;
+	char			*dst;
+
+	j = 0;
+	if (!s || !*s || !len)
+		return (NULL);
+	temp = ft_memalloc(len);
+	dst = temp;
+	while (j < len)
+	{
+		temp[j] = s[start];
+		j++;
+		start++;
+	}
+	temp[j] = '\0';
+	return (dst);
+}
